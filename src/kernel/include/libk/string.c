b9609792fa99665e03a5cf4d5a87e2d5c79eb3c9
thepercentageguy <chica33sfm@gmail.com>
2024-10-06 00:56:15 +0200

    implement kmemcpy, kmemset, and kememcmp

--- src/kernel/include/libk/string.c
+++ src/kernel/include/libk/string.c
@@ -1,5 +1,32 @@
 #include <libk/string.h>
 
+#include <stdint.h>
+
+void *kmemcpy(void *dest, const void *src, size_t n)
+{
+    uint8_t *pdest = (uint8_t *)dest;
+    const uint8_t *psrc = (const uint8_t *)src;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        pdest[i] = psrc[i];
+    }
+
+    return dest;
+}
+
+void *kmemset(void *s, int c, size_t n)
+{
+    uint8_t *p = (uint8_t *)s;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        p[i] = (uint8_t)c;
+    }
+
+    return s;
+}
+
 void *kmemmove(void *dstptr, const void *srcptr, size_t size)
 {
     unsigned char *dst = (unsigned char *)dstptr;
@@ -17,6 +44,20 @@ void *kmemmove(void *dstptr, const void *srcptr, size_t size)
     }
 
     return dstptr;
+}
+
+int kmemcmp(const void *s1, const void *s2, size_t n)
+{
+    const uint8_t *p1 = (const uint8_t *)s1;
+    const uint8_t *p2 = (const uint8_t *)s2;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (p1[i] != p2[i])
+            return p1[i] < p2[i] ? -1 : 1;
+    }
+
+    return 0;
 }
 
 size_t kstrlen(const char *str)
--- src/kernel/include/libk/string.h
+++ src/kernel/include/libk/string.h
@@ -3,7 +3,10 @@
 
 #include <stddef.h>
 
+void *kmemcpy(void *dest, const void *src, size_t n);
+void *kmemset(void *s, int c, size_t n);
 void *kmemmove(void *dstptr, const void *srcptr, size_t size);
+int kmemcmp(const void *s1, const void *s2, size_t n);
 
 size_t kstrlen(const char *str);
 char *kstrcat(char *dest, const char *src);
